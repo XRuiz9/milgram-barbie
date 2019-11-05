c4b26b806ac61ec9a32dea59ca4c312cd5867f07
XRuiz9 <xdruiz92@gmail.com>
2019-11-04 22:06:45 -0500

    Cleaned and finalize code

--- puppet/puppet.ino
+++ puppet/puppet.ino
@@ -1,22 +1,25 @@
 #include <ESP32Servo.h>
 
-Servo eyes;  // create servo object to control a servo
+// Create servo objects to control eye and finger motors
+Servo eyes;
 Servo fingers;
 
-// 16 servo objects can be created on the ESP32
-
-int pos = 0;    // variable to store the servo position
-// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33 
+// Declare pins
 int eyesPin = 18;
 int fingersPin = 14;
 int switchPin = 25;
 
+// Declare value that we are reading
+int switchValue = 0;
+
 void setup() {
   Serial.begin(9600);
-  
-  eyes.setPeriodHertz(50);    // standard 50 hz servo
-  eyes.attach(eyesPin); // attaches the servo on pin 18 to the servo object
 
+  //Initialize eye motor
+  eyes.setPeriodHertz(50);
+  eyes.attach(eyesPin);
+
+  //Initialize finger motor
   fingers.setPeriodHertz(50);
   fingers.attach(fingersPin);
   
@@ -24,44 +27,25 @@ void setup() {
 }
 
 void loop() {
+  // Read switch value
   switchValue = analogRead(switchPin);
-  Serial.println(switchValue);
-  
+
+  // Set default eye and finger position 
   eyes.write(120);
   fingers.write(120);
   delay(5);
 
+  // If switch is on, shock
   if (switchValue >= 4000) {
     shock();
   }
 
-//  int val1 = random(180);
-//  int val2 = random(180);
-//  int max, min;
-//  Serial.print(val1 + " " + val2);
-//  if (val1 > val2) {
-//    max = val1;
-//    min = val2;
-//  } else {
-//    max = val2;
-//    min = val1;
-//  }
-//  
-//  for (pos = 0; pos <= max; pos += 1) { // goes from 0 degrees to 180 degrees
-//    // in steps of 1 degree
-//    myservo.write(pos);    // tell servo to go to position in variable 'pos'
-//    delay(5);             // waits 15ms for the servo to reach the position
-//  }
-//  for (pos = 180; pos >= min; pos -= 1) { // goes from 180 degrees to 0 degrees
-//    myservo.write(pos);    // tell servo to go to position in variable 'pos'
-//    delay(5);             // waits 15ms for the servo to reach the position
-//  }
-}
-
 void shock() {
+  // Make eyes and fingers flutter
   eyes.write(30);
   fingers.write(30);
   delay(5);
+  
   eyes.write(0);
   fingers.write(0);
   delay(5);
